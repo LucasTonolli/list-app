54a83294accd015e645add215a7ed9cf61d9d53c
Lucas Tonolli <lucastonolli973@gmail.com>
2026-01-10 14:11:55 -0300

    refactor: minor changes in `ListDetail`

--- src/views/ListDetail.vue
+++ src/views/ListDetail.vue
@@ -7,12 +7,14 @@ import FloatingAddButton from '@/components/FloatingAddButton.vue';
 import AddListItem from '@/components/dialogs/AddListItem.vue';
 import { computed, ref } from 'vue';
 import ListItemRow from '@/components/ListItem/ListItemRow.vue';
+import type { ListItem } from '@/types/ListItem';
 
 
 const { getListById, toggleItem, removeItem, addItem } = useLists()
 const route = useRoute()
 const listId = computed(() => String( route.params.id))
 const list = computed(() => getListById(listId.value))
+const items = computed(() => list.value?.items ?? [])
 const dialog = ref<InstanceType<typeof AddListItem> | null>(null)
 const emit = defineEmits<{
   (e: 'toggle-item', isChecked: boolean): void
@@ -29,22 +31,31 @@ function handleCreateItem(payload: { name: string; description: string|null }): 
   emit('create-item')
 }
 
+function handleToggleItem(item: ListItem): void {
+  toggleItem(listId.value, item.id);
+  emit('toggle-item', item.checked)
+}
+
+function handleRemoveItem(item: ListItem): void {
+  removeItem(listId.value, item.id)
+  emit('remove-item')
+}
+
 </script>
 
 <template>
   <section class="">
     <div class="list-container">
       <ListItemRow
-        v-for="item in list?.items"
+        v-for="item in items"
         :key="item.id"
         :item="item"
-        :list-id="listId"
-        @toggle="toggleItem(listId, item.id); emit('toggle-item', item.checked)"
-        @remove="removeItem(listId, item.id); emit('remove-item')"
+        @toggle="handleToggleItem(item)"
+        @remove="handleRemoveItem(item)"
       />
     </div>
 
-    <p v-if="list?.items.length === 0" class="empty">
+    <p v-if="items.length === 0" class="empty">
       Sem itens na lista
     </p>
 
