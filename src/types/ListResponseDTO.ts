251fcec912ca5be103ef6bc534d98e0dc5f186c5
Lucas Tonolli <lucastonolli973@gmail.com>
2026-02-12 15:29:37 -0300

    refactor: change the type of property

--- src/types/ListResponseDTO.ts
+++ src/types/ListResponseDTO.ts
@@ -1,10 +1,10 @@
-import type { ListItem } from "./ListItem";
+import type { ListItemResponse } from "./ListItemResponseDTO";
 
 export interface ListResponse {
     uuid: string;
     title: string;
     items_count: number;
-    items: Array<ListItem>;
+    items: ListItemResponse[];
     shared_with_count: number;
     created_at: string;
     updated_at: string;
