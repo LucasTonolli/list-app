6c957b871f5a87bf50c8e8108bc124661f862727
Lucas Tonolli <lucastonolli973@gmail.com>
2026-08-07 17:09:33 -0300

    refactor: add nullsafe

--- src/api/client.ts
+++ src/api/client.ts
@@ -20,7 +20,7 @@ client.interceptors.request.use((config) => {
 client.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (error.response.status === 401) {
+    if (error.response?.status === 401) {
       localStorage.removeItem("token");
     }
 
